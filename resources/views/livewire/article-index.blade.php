d8c3e8212d5793c4c5b0c395668833dbb20bd7d4
Jeremy McPeak <jwmcpeak@gmail.com>
2024-09-06 22:38:28 -0500

    episode 27

--- resources/views/livewire/article-index.blade.php
+++ resources/views/livewire/article-index.blade.php
@@ -1,7 +1,15 @@
 <div class="m-auto w-1/2">
+    <div wire:offline>
+        You are offline
+    </div>
+
+
     @foreach ($articles as $article)
     <div class="mt-5 p-3" wire:key="{{$article->id}}">
-        <h3 class="text-2xl text-blue-500 mb-3 hover:text-blue-700">
+        <h3 class="text-2xl text-blue-500 mb-3 hover:text-blue-700"
+            wire:offline.class.remove="text-blue-500 hover:text-blue-700"
+            wire:offline.class="bg-red-500"
+        >
             <a href="/articles/{{$article->id}}">{{$article->title}}</a>
         </h3>
         <p>
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,14 +1,15 @@
 <div>
     <form>
         <div class="mt-2">
-            <input 
-                type="text" 
+            <input
+                type="text"
                 class="p-4 w-full border rounded-md bg-gray-700 text-white"
                 wire:model.live.debounce="searchText"
                 placeholder="{{$placeholder}}"
+                wire:offline.attr="disabled"
             >
         </div>
     </form>
     <livewire:search-results :results="$results" :show="!empty($searchText)">
-   
+
 </div>
